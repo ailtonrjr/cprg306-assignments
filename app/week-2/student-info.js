4d97206a5c936ff02554722b78d3d4c327740309
ailtonrjr <143243289+ailtonrjr@users.noreply.github.com>
2025-01-21 18:55:57 -0700

    Refining and finishing the assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Ailton Ribeiro Junior</p>
+      <p>
+        <Link href="https://github.com/ailtonrjr">https://github.com</Link>
+      </p>
+    </main>
+  );
+}
