70bab5ec6565fc9a369b74bbfa6115428a6466c5
ailtonrjr <143243289+ailtonrjr@users.noreply.github.com>
2025-02-18 23:59:14 -0700

    week-5 exercise commit

---
+++ app/week-5/page.js
@@ -0,0 +1,9 @@
+import Counter from "./new-item";
+
+export default function Page() {
+  return (
+    <main className="flex justify-center items-start h-screen bg-black">
+      <Counter />
+    </main>
+  );
+}
