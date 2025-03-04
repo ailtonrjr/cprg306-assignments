25a6d8c5b29d6a2c51ca7086448a49d78e762b57
ailtonrjr <143243289+ailtonrjr@users.noreply.github.com>
2025-03-03 17:40:04 -0700

    first implementations for week-6

---
+++ app/week-6/page.js
@@ -0,0 +1,9 @@
+import item from "./item.js";
+
+export default function Page() {
+  return (
+    <main className="flex justify-center items-start h-screen bg-black">
+      <item />
+    </main>
+  );
+}
