0176e688301d69e48d6df28b52565e07f61b1bad
Ailton Ribeiro Junior <ailtonrjr@Mac.lan>
2025-03-11 17:48:43 -0600

    some improvements on home page

--- app/page.js
+++ app/page.js
@@ -3,10 +3,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
-        Link to week 2: <Link href="week-2">Week 2 Page</Link>
+      <section className="flex flex-col bg-slate-950 p-4">
+      <h1 className="font-bold text-3xl text-white p-4">CPRG 306: Web Development 2 - Assignments</h1>
+      <p className="text-white">
+        Link to week 2: 
       </p>
+      <Link className="text-white" href="week-2">Week 2 Page</Link>
+      <Link className="text-white" href="week-3">Week 3 Page</Link>
+      <Link className="text-white" href="week-4">Week 4 Page</Link>
+      <Link className="text-white" href="week-5">Week 5 Page</Link>
+      <Link className="text-white" href="week-6">Week 6 Page</Link>
+      </section>
     </main>
   );
 }
