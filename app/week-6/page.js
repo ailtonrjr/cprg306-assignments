dc6d39a771c0d499f215b6507d4358c4a5910808
ailtonrjr <143243289+ailtonrjr@users.noreply.github.com>
2025-03-11 11:13:51 -0600

    some work on weeks 7 and 8

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,9 +1,11 @@
 import item from "./item.js";
+import ItemList from "./item-list.js";
 
 export default function Page() {
   return (
-    <main className="flex justify-center items-start h-screen bg-black">
-      <item />
+    <main className=" flex flex-col bg-slate-950 p-4">
+      <h1 className="text-3xl font-bold  text-gray-100">Shopping List</h1>
+      <ItemList />
     </main>
   );
 }
